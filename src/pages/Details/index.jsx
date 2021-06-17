5f70be0fb9c02ebd54310d5992de06f15a012a3c
Débora Rodrigues <44413380+dehrs@users.noreply.github.com>
2021-06-17 18:00:48 -0300

    tirando importação sem uso

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useRouteMatch } from "react-router-dom"
 import { api } from "../../services/api";
-import axios from "axios";
 
 import './styles.scss';
 
